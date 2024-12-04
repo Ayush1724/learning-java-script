69c769762a7f46a367ca8931fb558bc1c601bcaf
Ayush1724 <ayushshrivastava2015@gmail.com>
2024-12-04 19:55:41 +0530

    Success

---
+++ var.js
@@ -0,0 +1,16 @@
+// var a=10
+// var a=7
+// console.log(a);
+
+// let b=12
+// b=90
+// console.log(b);
+
+// const c=89
+// console.log(c);
+// d=a+b
+// console.log(d);
+//=================================================================
+let num1 = 10
+let num2 = 90
+console.log("num1+num2=", num1+num2);
