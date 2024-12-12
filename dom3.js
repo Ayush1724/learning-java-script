2a9d21c2b041d0278e571fb030a0f12ab7aa10a7
Ayush1724 <ayushshrivastava2015@gmail.com>
2024-12-12 20:00:54 +0530

    Success

---
+++ dom3.js
@@ -0,0 +1,14 @@
+let l = document.getElementsByTagName("li")
+
+// l[0].style.backgroundColor="yellow"
+// l[2].style.backgroundColor="yellow"
+// l[4].style.backgroundColor="yellow"
+// l[6].style.backgroundColor="yellow"
+// l[8].style.backgroundColor="yellow"
+// l[0].style.backgroundColor="yellow"
+// l[10].style.backgroundColor="yellow"
+for(let a=1;a<=10;a++){
+    if(a%2==1){
+        l[i].style.backgroundColor="yellow"
+    }
+}
