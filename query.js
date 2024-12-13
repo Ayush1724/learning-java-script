c52111320525f572c8bb6c1787505c2ca265079e
Ayush1724 <ayushshrivastava2015@gmail.com>
2024-12-13 19:52:09 +0530

    Success

---
+++ query.js
@@ -0,0 +1,11 @@
+let h = document.querySelector("#head")
+h.style.color="blue"
+
+let h2 = document.querySelector(".head2")
+h2.style.color="red"
+
+let h3=document.getElementsByClassName("head3")
+h3[0].textContent="Ayush"
+
+let h4= document.querySelectorAll("li")
+h4[1].textContent="Shrivastava"
